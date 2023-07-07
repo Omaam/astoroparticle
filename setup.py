093b10a0d3a7a37b72cd4ceea17b304221b27ac6
Omaam <omama.tomoki@gmail.com>
2023-07-07 10:47:04 +0900

    amend setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+"""Setup.
+"""
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
@@ -5,8 +8,12 @@ setup(
     version="0.0.1",
     description="Xspec inference with particle filter",
     author="Tomoki Omama",
+    packages=find_packages(),
+    install_requires=[
+        "tensorflow",
+        "tensorflow_probability"
+    ],
     classifiers=[
         "Development Status :: 1 - Planning"
     ],
-    packages=["partical_xspec"]
 )
