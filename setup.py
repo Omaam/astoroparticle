6a7294cb339d59c5fcb9478f806cc2bcf8ad20c1
Omaam <omama.tomoki@gmail.com>
2023-07-07 10:24:34 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="partical_xspec",
+    version="0.0.1",
+    description="Xspec inference with particle filter",
+    author="Tomoki Omama",
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    packages=["partical_xspec"]
+)
