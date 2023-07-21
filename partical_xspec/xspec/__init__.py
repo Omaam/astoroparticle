d4334f02e559506a1f45f20156f842f05768efa6
Omaam <omama.tomoki@gmail.com>
2023-07-21 16:35:15 +0900

    prepare loading response file

--- partical_xspec/xspec/__init__.py
+++ partical_xspec/xspec/__init__.py
@@ -1,3 +1,5 @@
 """Xspec managing library.
 """
 from partical_xspec.xspec.settings import set_energy
+from partical_xspec.xspec.settings import set_response
+from partical_xspec.xspec.settings import set_response_satellite_default
