80d1f056b80c7e91dbd876077ad93fe1eeba6421
Omaam <omama.tomoki@gmail.com>
2023-08-10 15:16:31 +0900

    delete xspec library

--- astroparticle/python/__init__.py
+++ astroparticle/python/__init__.py
@@ -5,6 +5,5 @@ from astroparticle.python import observations
 from astroparticle.python import particle_filter
 from astroparticle.python import test
 from astroparticle.python import transitions
-from astroparticle.python import xspec
 from astroparticle.python.particle_filter import ParticleFilter
 from astroparticle.python.particle_filter import WeightedParticleNumpy
--- astroparticle/python/xspec/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Xspec managing library.
-"""
-from astroparticle.python.xspec.settings import set_energy
-from astroparticle.python.xspec.settings import set_response
-from astroparticle.python.xspec.settings import set_response_satellite_default
