8977eec0db7153eeaeb76d1b46808105740704f2
Omaam <omama.tomoki@gmail.com>
2023-08-14 16:27:39 +0900

    add 'sum' for transition

--- astroparticle/python/transitions/__init__.py
+++ astroparticle/python/transitions/__init__.py
@@ -2,5 +2,6 @@
 """
 from astroparticle.python.transitions import util
 from astroparticle.python.transitions.constant_offset import ConstantOffset
+from astroparticle.python.transitions.sum import Sum
 from astroparticle.python.transitions.trend import Trend
 from astroparticle.python.transitions.vector_autoregressive import VectorAutoregressive
