832d375f74573992db0ac4a2d46a5164d88d4e15
Omaam <omama.tomoki@gmail.com>
2023-08-14 16:28:21 +0900

    add constant_offset

--- astroparticle/python/transitions/constant_offset.py
+++ astroparticle/python/transitions/constant_offset.py
@@ -7,7 +7,10 @@ from astroparticle.python.transitions.transition import Transition
 
 
 class ConstantOffset(Transition):
-    def __init__(self, constant_offset, dtype=tf.float32):
+    def __init__(self,
+                 constant_offset,
+                 dtype=tf.float32,
+                 name="ConstantOffset"):
         """Constant offset transition class.
 
         Args:
@@ -17,23 +20,30 @@ class ConstantOffset(Transition):
                 from the component models.
             dtype: The type of an element in the resulting Tensor.
         """
-        constant_offset = tf.convert_to_tensor(
-            constant_offset, dtype=dtype,
-            name="constant_offset")
+        with tf.name_scope(name):
+            constant_offset = tf.convert_to_tensor(
+                constant_offset, dtype=dtype,
+                name="constant_offset_value")
 
-        self.constant_offset = constant_offset
-        self.latent_size = constant_offset.shape[-1]
-        self.dtype = dtype
+            self.constant_offset = constant_offset
+            self.latent_size = constant_offset.shape[-1]
+            self.dtype = dtype
 
     def _default_latent_indicies(self):
         latent_indicies = tf.range(self.latent_size)
         return latent_indicies
 
     def _get_function(self):
-        means = self.constant_offset
-        scale_diag = tf.zeros(self.latent_size, dtype=self.dtype)
-
         def _transition_fn(i, x):
+            batch_shape = x.shape[:-1]
+            means = tf.broadcast_to(
+                self.constant_offset,
+                tf.concat([batch_shape, [self.latent_size]], axis=-1)
+            )
+            scale_diag = tf.zeros(
+                tf.concat([batch_shape, [self.latent_size]], axis=-1),
+                dtype=self.dtype)
+
             transition_dist = tfd.MultivariateNormalDiag(
                 means, scale_diag=scale_diag)
             return transition_dist
